f251b8e155c32cf061ca7baa8654200662c34df8
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-06-21 15:59:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,13 @@
 def oxford_comma(array)
-  if array.size <= 1 do 
+  if array.size <= 1 do
 output =  array.join
 return output
+end
 end
 
 if array.size == 2 do
   output =  array.join(" and ")
   return output
+end
 end
 end
